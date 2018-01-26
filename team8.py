22026756b500ef984d8cf9b9e36f33243a2fb269
ColinBolduc <300922@shakopeeschools.org>
2018-01-26 13:18:42 -0600

    Added team info
    Name + Strat + desc.

--- team8.py
+++ team8.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Forks' # Only 10 chars displayed.
+strategy_name = '50/50 revenge'
+strategy_description = '50/50 chance of C or B, Always B the round after other player goes B.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
