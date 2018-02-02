090e68d22fc3018ffbe8bb37d03e329fa51af93b
jordanxthexkid <31998726+jordanxthexkid@users.noreply.github.com>
2018-02-02 13:02:51 -0600

    123

--- team11.py
+++ team11.py
@@ -12,23 +12,16 @@ strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
-    
-    opponents_last_5moves = their_history[-1:5]
-    
+        
     if len(their_history)>= 3:
         if (their_history)== 'bbb':
             return 'b'
     else: 
         return 'b'
     
-    
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
+def opponents_last_5moves():
+    if 1==1:
+        their_history[-1:5]
            
     
 def test_move(my_history, their_history, my_score, their_score, result):
