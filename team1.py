73db35509208b761b6183de2de51566b02d48034
jdbjelland <222523@c1mrx7u0h3qd.isd720.com>
2018-02-05 13:17:16 -0600

    Team 1 Karma Strategy
    Final version of team 1's strategy.

--- team1.py
+++ team1.py
@@ -4,12 +4,20 @@
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
+#	  This is a test change wow
 ####
+import random
 
-team_name = 'Global Karma' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
-    
+team_name = 'GloblKarma' # Only 10 chars displayed.
+strategy_name = 'Karma'
+strategy_description = 'If you win, stay. If you lose, switch.'
+
+def flip():
+    if random.randint(1, 101) >=50:
+        return 'b'
+    else:
+        return 'c'
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,52 +25,14 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
+    if len(my_history)==0: #First round collude
+        return 'c'
+    if my_history[-1]=='c' and their_history[-1]=='b': #If we lost, counter back with a 50% chance 'b' or 'c'.
+        flip()
+    if my_history[-1]=='b' and their_history[-1]=='c': #If we won, keep the same strategy
+        return 'b'
+    if my_history[-1]=='c' and their_history[-1]=='c':# If both sides colluded, keep colluding.
+        return 'c' 
     else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
+        return 'b'
+    
