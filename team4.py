f3b79f214ea8fbf2f33d8cb7a56bf5837e0aaaf7
TheMarkOfN <230228@c1mpd5cbg943.isd720.com>
2018-02-05 13:09:48 -0600

    Add strategy description

--- team4.py
+++ team4.py
@@ -8,7 +8,7 @@
 
 team_name = 'GangstaliciousSmores' # Only 10 chars displayed.
 strategy_name = 'The Humiliator'
-strategy_description = 'How does this strategy decide?'
+strategy_description = 'First return c, then adapts if their_history has any b'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
